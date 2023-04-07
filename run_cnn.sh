d5ddce3c663be0e0b4c6e7f539a76bc48e01fa50
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-08 01:07:04 +0900

    Update run_cnn.sh

--- run_cnn.sh
+++ run_cnn.sh
@@ -1,3 +1,4 @@
 model='cnn'
 dt=`date '+%Y%m%d_%H%M%S'`
+mkdir ./result_model
 python train.py --model_fn ./result_model/review.native.${model}.${dt}.pth --train_fn ./data/review.sorted.uniq.refined.tok.shuf.train.tsv --gpu_id 2 --batch_size 128 --n_epochs 10 --word_vec_size 256 --dropout .3 --${model} --hidden_size 512 --n_layers 4 --window_size 3 4 5 6 7 8 --n_filters 128 128 128 128 128
