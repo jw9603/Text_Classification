e5edd4002f196d820cd4645820ebbf8c900bc7d5
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-08 01:07:21 +0900

    Update run_rnn.sh

--- run_rnn.sh
+++ run_rnn.sh
@@ -1,3 +1,4 @@
 model='rnn'
 dt=`date '+%Y%m%d_%H%M%S'`
+mkdir ./result_model
 python train.py --model_fn ./result_model/review.native.${model}.${dt}.pth --train_fn ./data/review.sorted.uniq.refined.tok.shuf.train.tsv --gpu_id 2 --batch_size 128 --n_epochs 10 --word_vec_size 256 --dropout .3 --${model} --hidden_size 512 --n_layers 4
