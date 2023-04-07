eefbc12f578f7e92648a7a1a2ac9a1b131b7973d
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-08 00:52:24 +0900

    Code to execute data tokenization using mecab,used when using rnn, cnn models

---
+++ tokenize.sh
@@ -0,0 +1,9 @@
+FN=$1
+TARGET_FN=$2
+
+cut -f1 ${FN} > ${FN}.label
+cut -f2 ${FN} | mecab -O wakati -b 20000 > ${FN}.text.tok
+
+paste ${FN}.label ${FN}.text.tok > ${TARGET_FN}
+
+# bash tokenize.sh review.sorted.uniq.refined.tsv review.sorted.uniq.refined.tok.tsv
