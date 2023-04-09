23fe5206db53bbe212de6e65a25892a1356090e4
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-09 22:30:24 +0900

    ensemble rnn + cnn

---
+++ run.sh
@@ -0,0 +1,2 @@
+dt=`date '+%Y%m%d_%H%M%S'`
+python train.py --model_fn ./result_model/model.${dt}.pth --train_fn ./data/review.sorted.uniq.refined.tok.shuf.train.tsv --gpu_id 2 --batch_size 128 --n_epochs 10 --word_vec_size 256 --dropout .3 --rnn --hidden_size 512 --n_layers 4 --cnn --window_size 3 4 5 6 7 8 --n_filters 128 128 128 128 128
