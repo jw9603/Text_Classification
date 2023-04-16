6995edfb1dad503874bb69846aadc196b41c044d
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-16 22:42:49 +0900

    Update make_test_bert.sh

--- make_test_bert.sh
+++ make_test_bert.sh
@@ -1,1 +1,1 @@
-cat ./data/review.sorted.uniq.refined.shuf.test.tsv | shuf | head -n 20 | awk -F'\t' '{print $2}' | python classify_plm.py --model_fn ./result_model/review.native.kcbert.20230406_231408.pth --gpu_id 0 | awk -F'\t' '{print $1}' > ./model/review.native.kcbert.20230406_231408.pth.result.txt
+cat ./data/review.sorted.uniq.refined.shuf.test.tsv | awk -F'\t' '{print $2}' | python classify_plm.py --model_fn ./result_model/review.native.kcbert.20230406_231408.pth --gpu_id 0 | awk -F'\t' '{print $1}' > ./model/review.native.kcbert.20230406_231408.pth.result.txt
