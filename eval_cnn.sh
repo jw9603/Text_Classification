3495f3ab167dffd38ac91a257651efde6fe2613f
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-16 16:42:06 +0900

    View test data results at a glance

---
+++ eval_cnn.sh
@@ -0,0 +1,3 @@
+## look result in-person
+# 모델 인자는 ensemble 모델이므로 rnn만의 성능을 보기 위해서는 drop_rnn인자 사용
+cut -f2 ./data/review.sorted.uniq.refined.shuf.test.tsv | shuf | head -n 20 | python ./classify.py --model_fn ./result_model/model.20230415_232506.pth --gpu_id 0 --drop_rnn
