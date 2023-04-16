89929fb62dbd5d2944759b23ad7cb843ed0782d4
Jiwon Jeong <70795645+jw9603@users.noreply.github.com>
2023-04-16 22:43:32 +0900

    Update make_test_rnn.sh

--- make_test_rnn.sh
+++ make_test_rnn.sh
@@ -1,1 +1,1 @@
-cat ./data/review.sorted.uniq.refined.shuf.test.tsv | shuf | awk -F'\t' '{print $2}' | python classify.py --model_fn ./result_model/model.20230415_232506.pth --gpu_id 0 --drop_cnn | awk -F'\t' '{print $1}' > ./model/model.202301415_232506_rnn.pth.result.txt
+cat ./data/review.sorted.uniq.refined.shuf.test.tsv | awk -F'\t' '{print $2}' | python classify.py --model_fn ./result_model/model.20230415_232506.pth --gpu_id 0 --drop_cnn | awk -F'\t' '{print $1}' > ./model/model.202301415_232506_rnn.pth.result.txt
